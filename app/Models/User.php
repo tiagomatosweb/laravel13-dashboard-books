1b5eac724550516d9df493aad7a0be75461f331d
Tiago Matos <tiago@tiagomatos.com>
2026-04-28 14:16:21 +1000

    Aula 09

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+// Eloquent
+// ORM - Object Relation Mapping
+
+// CRUD - Create Read Update Delete
+// Create => User::create(['name' => 'Jon', 'email' => 'jon@snow.com', 'password' => '123456']);
+// Read => User::find(1);
+// Update => User::update(['name' => 'Fulano'])
+// Delete => User::delete(1);
+
+
+// #[Fillable(['name', 'email', 'password'])]
+// #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
@@ -25,8 +38,8 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'email_verified_at' => 'datetime', // Carbon
+            'password' => 'hashed', // Hash
         ];
     }
 }
